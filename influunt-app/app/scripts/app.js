41f3fc944bacff71dda239dc0be7a3d81e2025d9
Paulo Fernandes <phfernandespereira@gmail.com>
2016-06-03 16:03:07 -0300

    corrige erros codacy do projeto bootstraped

--- influunt-app/app/scripts/app.js
+++ influunt-app/app/scripts/app.js
@@ -21,24 +21,24 @@ angular
   .config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
 
-      $urlRouterProvider.otherwise("/index/main");
+      $urlRouterProvider.otherwise('/index/main');
 
       $stateProvider
         .state('index', {
           abstract: true,
-          url: "/index",
-          templateUrl: "views/common/content.html",
+          url: '/index',
+          templateUrl: 'views/common/content.html',
           controller: 'MainCtrl',
           controllerAs: 'main'
         })
         .state('index.main', {
-          url: "/main",
-          templateUrl: "views/main.html",
+          url: '/main',
+          templateUrl: 'views/main.html',
           data: { pageTitle: 'Example view' },
         })
         .state('index.minor', {
-          url: "/minor",
-          templateUrl: "views/minor.html",
+          url: '/minor',
+          templateUrl: 'views/minor.html',
           data: { pageTitle: 'Example view' }
         });
     }])
