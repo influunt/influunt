bf6a5c7ac2fef63771e764b4bd81cc101dd6aa23
Paulo Fernandes <phfernandespereira@gmail.com>
2016-06-07 18:30:48 -0300

    adiciona feature cucumber ao projeto

---
+++ influunt-app/features/login.feature
@@ -0,0 +1,33 @@
+# language: pt
+
+Funcionalidade: Usuário fazendo login
+  Como um usuário não logado
+  Com o objetivo de entrar no sistema Influunt
+  O usuário deve informar login e senha para conseguir acesso ao sistema.
+
+  Cenário: Tentativa de acesso com usuário não informado
+    Dado que o usuário não esteja logado
+    Quando o usuário acessa a tela de login
+    E informa somente o nome do usuário
+    Então o usuário não deve conseguir acessar o sistema
+    E o sistema deve informar "Campo obrigatório" para o campo de senha
+
+  Cenário: Tentavia de acesso com senha não informada
+    Dado que o usuário não esteja logado
+    Quando o usuário acessa a tela de login
+    E informa somente a senha
+    Então o usuário não deve conseguir acessar o sistema
+    E o sistema deve informar "Campo obrigatório" para o campo de usuário
+
+  Cenário: Tentativa de acesso com senha ou usuário incorretos
+    Dado que o usuário não esteja logado
+    Quando o usuário acessa a tela de login
+    E informa usuário ou senha incorretos
+    Então o usuário não deve conseguir acessar o sistema
+    E o sistema deve informar "Usuário ou senha incorretos"
+
+  Cenário: Tentativa de acesso com usuário e senha corretos
+   Dado que o usuário não esteja logado
+   Quando o usuário acessa a tela de login
+   E informa usuário e senha corretos
+   Então o usuário deve ser enviado para a tela de dashboard
---
+++ influunt-app/features/step_definitions/login.steps.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = function() {
+
+  this.Given(/^que o usuário não esteja logado$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^o usuário acessa a tela de login$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^informa somente o nome do usuário$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^o usuário não deve conseguir acessar o sistema$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^o sistema deve informar "([^"]*)" para o campo de senha$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^informa somente a senha$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^o sistema deve informar "([^"]*)" para o campo de usuário$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^informa usuário ou senha incorretos$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^o sistema deve informar "([^"]*)"$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^informa usuário e senha corretos$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+  this.Given(/^o usuário deve ser enviado para a tela de dashboard$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback(null, 'pending');
+  });
+
+};
