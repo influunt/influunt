33e908e454ec8c619c92224ead9fb7064a5df15d
lesio <lesio.lap@gmail.com>
2016-06-30 20:00:21 -0300

    cucumber fabricantes

---
+++ influunt-app/features/run_cucumber_tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cucumber.js features/cidades/crud_cidades.feature features/areas/crud_areas.feature features/configuracoes_controladoresØbower.json/crud_configuracoes_controladores.feature features/fabricantes/crud_fabricantes.feature features
