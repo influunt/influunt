ef5a02a2553b05bed991104627beeab63acb2280
Paulo Fernandes <phfernandespereira@gmail.com>
2016-07-04 17:30:24 -0300

    fixes tests

--- influunt-app/test/spec/filters/limites-list-to-string.js
+++ influunt-app/test/spec/filters/limites-list-to-string.js
@@ -2,6 +2,7 @@
 
 describe('Filter: limitesListToString', function () {
 
+  var limites;
   // load the filter's module
   beforeEach(module('influuntApp'));
 
@@ -9,11 +10,36 @@ describe('Filter: limitesListToString', function () {
   var limitesListToString;
   beforeEach(inject(function ($filter) {
     limitesListToString = $filter('limitesListToString');
+    limites = [{
+      latitude: 123,
+      longitude: 456
+    },
+    {
+      latitude: 231,
+      longitude: 564
+    },
+    {
+      latitude: 321,
+      longitude: 654
+    }];
   }));
 
-  it('should return the input prefixed with "limitesListToString filter:"', function () {
-    var text = 'angularjs';
-    expect(limitesListToString(text)).toBe('limitesListToString filter: ' + text);
+  it('Não deve retornar nada caso não haja lista de limites definida', function() {
+    expect(limitesListToString()).not.toBeDefined();
+  });
+
+  it('Deve retornar uma string contendo três elementos de lista HTML', function() {
+    var result = limitesListToString(limites);
+    var resultHTML = $(result);
+
+    expect(result).toBeDefined();
+    expect(resultHTML.length).toBe(3);
+  });
+
+  it('Deve retornar uma lista de pares ordenados (x, y) da coordenada', function() {
+    expect(limitesListToString(limites)).toMatch(/(123; 456)/);
+    expect(limitesListToString(limites)).toMatch(/(231; 564)/);
+    expect(limitesListToString(limites)).toMatch(/(321; 654)/);
   });
 
 });
