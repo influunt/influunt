9ce6387f7a9eee0f36a15332d0149554e377295a
Paulo Fernandes <phfernandespereira@gmail.com>
2016-07-13 10:11:27 -0300

    realocacao da tela de planos

---
+++ influunt-app/app/scripts/controllers/planos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name influuntApp.controller:PlanosCtrl
+ * @description
+ * # PlanosCtrl
+ * Controller of the influuntApp
+ */
+angular.module('influuntApp')
+  .controller('PlanosCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
