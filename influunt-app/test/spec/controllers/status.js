f5866cf16ac477d946098db4cc115b5ef1036de9
Rodrigo Sol <rodrigosol@gmail.com>
2016-07-19 22:15:34 -0300

    Remove nonsense test

--- influunt-app/test/spec/controllers/status.js
+++ influunt-app/test/spec/controllers/status.js
@@ -17,7 +17,5 @@ describe('Controller: StatusCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(StatusCtrl.awesomeThings.length).toBe(3);
-  });
+  
 });
