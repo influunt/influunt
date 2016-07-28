0404d36e5538b9e3f62cbadd838a7f7b0204399b
lesio <lesio.lap@gmail.com>
2016-07-28 15:26:14 -0300

    unificar tela de fabricante e modelo de controlador

--- influunt-app/features/support/scripts/create_usuario.sql
+++ influunt-app/features/support/scripts/create_usuario.sql
@@ -1,3 +1,3 @@
 DELETE FROM `sessoes`;
 DELETE FROM  `usuarios`;
-INSERT INTO `usuarios` (`login`,`senha`, `email`, `nome`, `root`, `data_criacao`, `data_atualizacao`) VALUES ('root', '$2a$10$EzudGIqkxquJjLGawuMrOu9K6S28yc/R/YSAVxsvb5bSryOYWd5eq', 'root@influunt.com.br', 'Administrador Geral', 'true', CURRENT_TIME(), CURRENT_TIME());
+INSERT INTO `usuarios` (`id`, `login`, `senha`, `email`, `nome`, `root`, `data_criacao`, `data_atualizacao`) VALUES (RANDOM_UUID(), 'root', '$2a$10$EzudGIqkxquJjLGawuMrOu9K6S28yc/R/YSAVxsvb5bSryOYWd5eq', 'root@influunt.com.br', 'Administrador Geral', 'true', CURRENT_TIME(), CURRENT_TIME());
