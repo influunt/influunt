a38323e3ba0406c8f6a7fe6a827d5b15af95988d
leonardoherbert <leonardo@rarolabs.com.br>
2016-08-04 09:40:43 -0300

    limitar a criacao de grupos semaforicos

--- influunt-app/app/scripts/controllers/controladores/configuracao-grupos.js
+++ influunt-app/app/scripts/controllers/controladores/configuracao-grupos.js
@@ -41,7 +41,12 @@ angular.module('influuntApp')
       };
 
       $scope.adicionaGrupoSemaforico = function() {
-        var obj = { anel: {idJson: $scope.currentAnel.idJson}, idJson: UUID.generate() };
+        var obj = { 
+          idJson: UUID.generate(),
+          anel: {idJson: $scope.currentAnel.idJson}, 
+          tipo: "VEICULAR",
+          faseVermelhaApagadaAmareloIntermitente: false
+        };
 
         $scope.objeto.gruposSemaforicos = $scope.objeto.gruposSemaforicos || [];
         $scope.currentAnel.gruposSemaforicos = $scope.currentAnel.gruposSemaforicos || [];
@@ -76,6 +81,10 @@ angular.module('influuntApp')
       $scope.selecionaAnelGruposSemaforicos = function(index) {
         $scope.selecionaAnel(index);
         $scope.atualizaGruposSemaforicos();
+      };
+
+      $scope.podeAdicionarGrupoSemaforico = function(){
+        return $scope.objeto.gruposSemaforicos.length < $scope.objeto.limiteGrupoSemaforico;
       };
 
       // $scope.atualizaGruposSemaforicos = function() {
