7ce5820cf4fef42a112fbf40a38f74035427cd3e
Dante Marchi <dante@rarolabs.com.br>
2016-08-10 20:45:20 -0300

    horarios test

--- influunt-app/test/spec/controllers/horarios.js
+++ influunt-app/test/spec/controllers/horarios.js
@@ -17,7 +17,4 @@ describe('Controller: HorariosCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(HorariosCtrl.awesomeThings.length).toBe(3);
-  });
 });
