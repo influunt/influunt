c3d7f3bc447f057e178d2bcfc01f815f12097c8e
leonardoherbert <leonardo@rarolabs.com.br>
2016-09-05 12:56:01 -0300

    add ;

--- influunt-app/app/scripts/controllers/controladores.js
+++ influunt-app/app/scripts/controllers/controladores.js
@@ -439,8 +439,7 @@ angular.module('influuntApp')
       };
 
       $scope.podeAtivar = function(controlador) {
-        return (controlador.statusControlador === 'CONFIGURADO') && (controlador.versoesPlanos && controlador.versoesPlanos.length > 0) && (controlador.versoesTabelasHorarias && controlador.versoesTabelasHorarias.length > 0)
-
+        return (controlador.statusControlador === 'CONFIGURADO') && (controlador.versoesPlanos && controlador.versoesPlanos.length > 0) && (controlador.versoesTabelasHorarias && controlador.versoesTabelasHorarias.length > 0);
       }
 
     }]);
