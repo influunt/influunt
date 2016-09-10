5c2b6195567a9cb07c92623ce4cf9ee22e26c7ee
Paulo Fernandes <phfernandespereira@gmail.com>
2016-09-10 15:17:02 -0300

    temp fix dos indexes de controladores e auditorias

--- influunt-app/app/scripts/controllers/auditorias.js
+++ influunt-app/app/scripts/controllers/auditorias.js
@@ -8,10 +8,20 @@
  * Controller of the influuntApp
  */
 angular.module('influuntApp')
-  .controller('AuditoriasCtrl', ['$scope', '$controller',
-    function ($scope, $controller) {
+  .controller('AuditoriasCtrl', ['$scope', '$controller', 'Restangular',
+    function ($scope, $controller, Restangular) {
       // Herda todo o comportamento do crud basico.
       $controller('CrudCtrl', {$scope: $scope});
       $scope.inicializaNovoCrud('auditorias/');
+
+      // @todo       assim que a api implementar o mesmo modelo de paginação para controladores,
+      //             este metodo deverá voltar trabalhar com o metodo index padrao.
+      $scope.index = function() {
+        return Restangular.all('auditorias/').getList()
+          .then(function(res) {
+            $scope.lista = res;
+          });
+      };
+
     }]);
 
