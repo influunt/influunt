22d8c857bae959022e0869ac0ae72eb25f75ffe0
Dante Marchi <dante@rarolabs.com.br>
2016-09-27 20:41:11 -0300

    view, i18n, config e menu

---
+++ influunt-app/app/scripts/controllers/simulacao.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name influuntApp.controller:AuditoriasCtrl
+ * @description
+ * # AuditoriasCtrl
+ * Controller of the influuntApp
+ */
+angular.module('influuntApp')
+  .controller('SimulacaoCtrl', ['$scope', '$controller',
+    function ($scope, $controller) {
+      // Herda todo o comportamento do crud basico.
+      $controller('CrudCtrl', {$scope: $scope});
+      $scope.inicializaNovoCrud('simulacao');
+
+    }]);
