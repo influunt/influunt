448b5a98c58d51bd48417cfaafb827941a95407e
Juarez Lustosa <juarez.lustosa@gmail.com>
2016-10-11 13:19:41 -0300

    Método Scroll down criado

--- influunt-app/features/step_definitions/shared.steps.js
+++ influunt-app/features/step_definitions/shared.steps.js
@@ -40,6 +40,10 @@ module.exports = function() {
     return sharedSteps.realizarScrollUp();
   });
 
+  this.Given(/^o usuário realizar um scroll down$/, function() {
+    return sharedSteps.realizarScrollDown();
+  });
+
   this.Given(/^que o usuário selecione o anel (\d+)$/, function (numeroAnel) {
     return sharedSteps.trocarAnel(numeroAnel);
   });
