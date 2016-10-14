2bed32e94d4c7bf9dbf431c2ca514d03ff119e12
Lesio <lesio@rarolabs.com.br>
2016-10-14 11:09:19 -0300

    inclusao de permissoes

--- influunt-api/app/controllers/RelatoriosController.java
+++ influunt-api/app/controllers/RelatoriosController.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import be.objectify.deadbolt.java.actions.DeferredDeadbolt;
+import be.objectify.deadbolt.java.actions.Dynamic;
 import com.google.inject.Inject;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Security;
 import reports.AuditoriaReportService;
 import reports.ControladoresReportService;
 import reports.ReportType;
 import security.Auditoria;
+import security.Secured;
 import utils.InfluuntQueryBuilder;
 import utils.InfluuntQueryResult;
 
@@ -19,15 +23,18 @@ import java.util.concurrent.CompletionStage;
 /**
  * Created by lesiopinheiro on 04/10/16.
  */
+@DeferredDeadbolt
+@Security.Authenticated(Secured.class)
+@Dynamic("Influunt")
 public class RelatoriosController extends Controller {
 
     @Inject
     public AuditoriaReportService auditoriaReportService;
 
-
     @Inject
     public ControladoresReportService controladoresReportService;
 
+    @Dynamic(value = "Influunt")
     public CompletionStage<Result> gerarRelatorioAuditoria() {
         ReportType reportType = ReportType.valueOf(request().getQueryString("tipoRelatorio").toString());
 
@@ -41,6 +48,7 @@ public class RelatoriosController extends Controller {
         return CompletableFuture.completedFuture(ok(input).as(reportType.getContentType()));
     }
 
+    @Dynamic(value = "Influunt")
     public CompletionStage<Result> gerarRelatorioControladoresStatus() {
 
         ReportType reportType = ReportType.valueOf(request().getQueryString("tipoRelatorio").toString());
@@ -54,6 +62,7 @@ public class RelatoriosController extends Controller {
         return CompletableFuture.completedFuture(ok(input).as(reportType.getContentType()));
     }
 
+    @Dynamic(value = "Influunt")
     public CompletionStage<Result> gerarRelatorioControladoresFalhas() {
         ReportType reportType = ReportType.valueOf(request().getQueryString("tipoRelatorio").toString());
 
