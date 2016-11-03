c01573c8e4073c1d5b0e653fd7dace311e20e931
Paulo Fernandes <phfernandespereira@gmail.com>
2016-11-03 17:41:00 -0200

    fixes testes

--- influunt-app/test/spec/controllers/modelos.js
+++ influunt-app/test/spec/controllers/modelos.js
@@ -27,9 +27,10 @@ describe('Controller: ModelosCtrl', function () {
       limiteAnel: 6,
       limiteDetectorPedestre: 8,
       limiteDetectorVeicular: 10,
-      limiteTabelasEntreVerdes: 2
+      limiteTabelasEntreVerdes: 2,
+      limitePlanos: 2
     };
-    var texto = '<ul><li>modelos.limiteEstagio: 2</li><li>modelos.limiteGrupoSemaforico: 4</li><li>modelos.limiteAnel: 6</li><li>modelos.limiteDetectorPedestre: 8</li><li>modelos.limiteDetectorVeicular: 10</li><li>modelos.limiteTabelasEntreVerdes: 2</li></ul>';
+    var texto = '<ul><li>modelos.limiteEstagio: 2</li><li>modelos.limiteGrupoSemaforico: 4</li><li>modelos.limiteAnel: 6</li><li>modelos.limiteDetectorPedestre: 8</li><li>modelos.limiteDetectorVeicular: 10</li><li>modelos.limiteTabelasEntreVerdes: 2</li><li>modelos.limitePlanos: 2</li></ul>';
     expect(scope.limitesList(modelo)).toBe(texto);
   });
 });
