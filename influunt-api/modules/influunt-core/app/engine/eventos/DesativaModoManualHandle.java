073f9ad83b3d19997aa756ee7817d4b8387be175
leonardoherbert <leonardo@rarolabs.com.br>
2016-11-09 11:54:24 -0200

    fix testes modo manual

---
+++ influunt-api/modules/influunt-core/app/engine/eventos/DesativaModoManualHandle.java
@@ -0,0 +1,25 @@
+package engine.eventos;
+
+import engine.AgendamentoTrocaPlano;
+import engine.EventoMotor;
+import engine.GerenciadorDeEstagios;
+import engine.services.PlanoService;
+import models.Anel;
+import models.Plano;
+
+/**
+ * Created by leonardo on 11/7/16.
+ */
+public class DesativaModoManualHandle extends GerenciadorDeEventos{
+    @Override
+    protected void processar(EventoMotor eventoMotor) {
+        Anel anel = gerenciadorDeEstagios.getPlano().getAnel();
+        if (anel.isAceitaModoManual()) {
+            gerenciadorDeEstagios.trocarPlano(new AgendamentoTrocaPlano(null, plano, eventoMotor.getTimestamp()));
+        }
+    }
+
+    public DesativaModoManualHandle(GerenciadorDeEstagios gerenciadorDeEstagios) {
+        super(gerenciadorDeEstagios);
+    }
+}
