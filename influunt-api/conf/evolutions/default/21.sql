8c3f947ce07d321f524c144cb5829730efec634d
leonardoherbert <leonardo@rarolabs.com.br>
2016-11-21 17:02:49 -0200

    remove relacao detectores

--- influunt-api/conf/evolutions/default/21.sql
+++ influunt-api/conf/evolutions/default/21.sql
@@ -1,6 +1,8 @@
 # --- !Ups
+DROP index ix_detectores_controlador_id on detectores;
 ALTER TABLE detectores DROP COLUMN controlador_id;
 
 
 # --- !Downs
+ADD index ix_detectores_controlador_id on detectores;
 ALTER TABLE detectores ADD COLUMN controlador_id TINYINT(1);
