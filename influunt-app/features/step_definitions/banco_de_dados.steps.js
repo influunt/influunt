a030ef9280d5c2fa0ad342f66c56ca604b27a3b2
Juarez Lustosa <juarez.lustosa@gmail.com>
2016-12-14 19:06:06 -0200

    Closes #1632

--- influunt-app/features/step_definitions/banco_de_dados.steps.js
+++ influunt-app/features/step_definitions/banco_de_dados.steps.js
@@ -1,51 +1,59 @@
 'use strict';
 
-var InsertBanco = require('../support/page-objects/banco_de_dados.js');
+var Banco = require('../support/page-objects/banco_de_dados.js');
 
 module.exports = function() {
-  var insertBanco = new InsertBanco();
+  var banco = new Banco();
 
   this.Given(/^que o sistema possui ao menos um controlador cadastrado$/, function() {
-    return insertBanco.cadastrarControlador();
+    return banco.cadastrarControlador();
   });
 
   this.Given(/^que o sistema possua controladores cadastrados$/, function() {
-    return insertBanco.variosControladores();
+    return banco.controladores();
+  });
+
+  this.Given(/^que o sistema possua "([^"]*)" controladores cadastrados$/, function(total) {
+    var quantTotal = parseInt(total);
+    banco.removeControladores();
+    for (var i = 0; i < quantTotal; i++) {
+      banco.variosControladores();
+    }
   });
 
   this.Given(/^que o sistema possua controladores cadastrados e configurados$/, function() {
-    return insertBanco.variosControladoresConfigurados();
+    return banco.variosControladoresConfigurados();
   });
 
   this.Given(/^que o sistema possua planos para o controlador cadastrado$/, function() {
-    return insertBanco.cadastrarPlanoParaControlador();
+    return banco.cadastrarPlanoParaControlador();
   });
 
   this.Given(/^que o sistema possua tabela horária para o controlador cadastrado$/, function() {
-    return insertBanco.cadastrarTabelaHorariaParaControlador();
+    return banco.cadastrarTabelaHorariaParaControlador();
   });
 
   this.Given(/^for desabilitada no perfil visualizar todas as áreas$/, function () {
-    return insertBanco.desabilitarPermissoes();
+    return banco.desabilitarPermissoes();
   });
 
   this.Given(/^for abilitado no perfil visualizar todas as áreas$/, function () {
-    return insertBanco.desabilitarPermissoes();
+    return banco.desabilitarPermissoes();
   });
 
   this.Given(/^que exista ao menos um agrupamento cadastrado no sistema$/, function() {
-    return insertBanco.existeAoMenosUmAgrupamento();
+    return banco.existeAoMenosUmAgrupamento();
   });
 
   this.Given(/^que este controlador esteja finalizado$/, function() {
-    return insertBanco.finalizarControlador();
+    return banco.finalizarControlador();
   });
 
   this.Given(/^que o usuário não possa definir perfis$/, function () {
-    return insertBanco.desabilitarPermissoes();
+    return banco.desabilitarPermissoes();
   });
 
   this.Given(/^que possua controladores com áreas diferentes cadastrados$/, function () {
-    return insertBanco.controladoresAreasDiferentes();
+    return banco.controladoresAreasDiferentes();
   });
 };
