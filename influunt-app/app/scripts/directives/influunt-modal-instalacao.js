464befd5962f1a8897cadb2f81d504deaa22f5b3
Pedro Pires <pedro@rarolabs.com.br>
2016-12-14 18:57:04 -0200

    Download de arquivo de configuração do controlador fixes #1617

--- influunt-app/app/scripts/directives/influunt-modal-instalacao.js
+++ influunt-app/app/scripts/directives/influunt-modal-instalacao.js
@@ -7,8 +7,8 @@
  * # influuntModalTimeline
  */
 angular.module('influuntApp')
-  .directive('influuntModalInstalacao', ['Restangular', 'toast', '$filter', 'influuntBlockui',
-    function (Restangular, toast, $filter, influuntBlockui) {
+  .directive('influuntModalInstalacao', ['Restangular', 'toast', '$filter', 'influuntBlockui', '$http',
+    function (Restangular, toast, $filter, influuntBlockui, $http) {
       return {
         templateUrl: 'views/directives/influunt-modal-instalacao.html',
         restrict: 'E',
@@ -18,6 +18,8 @@ angular.module('influuntApp')
           title: '@'
         },
         link: function(scope) {
+          var prepareDownloadData;
+
           scope.instalacao = function(resource, id) {
             return Restangular.one(resource, id).all('instalacao').customGET()
               .then(function(res) {
@@ -26,6 +28,7 @@ angular.module('influuntApp')
                   publicKey: res.publicKey,
                   privateKey: res.privateKey
                 };
+                prepareDownloadData();
               })
               .catch(function(err) {
                 toast.error($filter('translate')('geral.mensagens.default_erro'));
@@ -41,6 +44,18 @@ angular.module('influuntApp')
           scope.$watch('resourceId', function(resourceId) {
             return resourceId && scope.instalacao(scope.resource, resourceId);
           });
+
+          prepareDownloadData = function() {
+            $http.get('/resources/controlador.conf').then(function(response) {
+              var config = response.data
+                  .replace('|CONTROLADOR_ID|', scope.instalacaoControlador.id)
+                  .replace('|CHAVE_PUBLICA|', scope.instalacaoControlador.publicKey)
+                  .replace('|CHAVE_PRIVADA|', scope.instalacaoControlador.privateKey);
+
+              var blob = new Blob([config], { type: 'text/plain' });
+              scope.downloadUrl = window.URL.createObjectURL(blob);
+            });
+          };
         }
       };
     }]);
