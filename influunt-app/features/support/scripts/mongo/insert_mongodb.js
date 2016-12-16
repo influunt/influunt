f467fc60d1b8626fcb89af8b8b455521814871aa
Juarez Lustosa <juarez.lustosa@gmail.com>
2016-12-16 21:57:32 -0200

    Fix review controladorfisicoid

--- influunt-app/features/support/scripts/mongo/insert_mongodb.js
+++ influunt-app/features/support/scripts/mongo/insert_mongodb.js
@@ -11,7 +11,7 @@ var NumberLong = Long.fromString('1479756629334');
 var MongoInsert = function () {
   var trocaPlanosControladores = [
     {'_id' : ObjectId('58334b5573835c48f22fb7a0'),
-     'idControlador' : '279d3e6e-b3ab-4e9f-8358-67e393e5ed0f',
+     'idControlador' : '66f6865f-6963-4ff5-b160-7c6febb68c03',
      'idAnel' : '043007e5-ee02-4383-bde1-87346abdc895',
      'timestamp' : NumberLong,
      'conteudo' : {'momentoDaTroca' : '21/11/2016 17:30:29',
@@ -27,7 +27,7 @@ var MongoInsert = function () {
   var alarmesFalhasControladores1 = [
     {
       '_id' : ObjectId('5833538c2d4e4bfb76af0d84'),
-      'idControlador' : '279d3e6e-b3ab-4e9f-8358-67e393e5ed0f',
+      'idControlador' : '66f6865f-6963-4ff5-b160-7c6febb68c03',
       'idAnel' : '043007e5-ee02-4383-bde1-87346abdc895',
       'recuperado' : false,
       'timestamp' : NumberLong,
@@ -67,6 +67,15 @@ var MongoInsert = function () {
     }
   ];
 
+  var statusControlador = [
+    {
+      '_id' : ObjectId('57dafa79dd605a97ce6003e4'),
+      'idControlador' : '3c802c00-6d7f-423d-b6e7-cfc8e744c8ae',
+      'timestamp' : NumberLong,
+      'conectado' : true
+    }
+  ];
+
   var clearDb = function(db, object, tableName, callback) {
     var id = object[0]._id;
 
@@ -95,6 +104,7 @@ var MongoInsert = function () {
     assert.equal(null, err);
     var tableTrocaPlanosControldaores = 'trocaPlanosControladores';
     var tableAlarmesFalhasControladores = 'alarmes_falhas_controladores';
+    var tableStatusControladores = 'status_conexao_controlador'
 
     clearDb(db, trocaPlanosControladores, tableTrocaPlanosControldaores, function() {
       db.close();
