4283ad7b36b19d337eb3526c0d21395f335051df
Rodrigo Sol <rodrigosol@gmail.com>
2017-01-11 14:45:37 -0200

    envio de endereco ip

--- influunt-api/modules/influunt-core/app/protocol/LerDadosControlador.java
+++ influunt-api/modules/influunt-core/app/protocol/LerDadosControlador.java
@@ -11,6 +11,7 @@ import models.Plano;
 import models.StatusDevice;
 import org.fusesource.mqtt.client.QoS;
 import org.joda.time.DateTime;
+import utils.IPUtils;
 
 import java.nio.charset.StandardCharsets;
 
@@ -58,6 +59,9 @@ public class LerDadosControlador {
         });
         controladorJson.putPOJO("planos", itens);
         controladorJson.putPOJO("dados", dados);
+
+        ArrayNode ips = controladorJson.putArray("ips");
+        IPUtils.listIps().stream().forEach(ip -> ips.add(ip));
 
         return new Envelope(TipoMensagem.LER_DADOS_CONTROLADOR, envelope.getIdControlador(), DestinoCentral.leituraDadosControlador(), QoS.AT_MOST_ONCE, controladorJson.toString(), envelope.getIdMensagem());
     }
---
+++ influunt-api/modules/influunt-core/app/utils/IPUtils.java
@@ -0,0 +1,53 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rodrigosol on 1/11/17.
+ */
+public class IPUtils {
+
+    public static List<String> listIps() {
+        List<String> ips = new ArrayList<>();
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            System.out.println(" IP Addr: " + localhost.getHostAddress());
+            // Just in case this host has multiple IP addresses....
+            InetAddress[] allMyIps = InetAddress.getAllByName(localhost.getCanonicalHostName());
+            if (allMyIps != null && allMyIps.length > 0) {
+                for (int i = 0; i < allMyIps.length; i++) {
+                    ips.add(allMyIps[i].toString());
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.out.println(" (error retrieving server host name)");
+        }
+
+
+        URL whatismyip = null;
+        try {
+            whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                whatismyip.openStream()));
+
+            String ip = in.readLine();
+            ips.add(ip);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ips;
+
+    }
+}
